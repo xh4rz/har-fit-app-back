462b7380e213f0b53153bf425598ee34329ddc1f
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-05-23 01:06:43 -0500

    feat: setup darraghor eslint-plugin-nestjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,46 @@
-// @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import prettierConfig from 'eslint-config-prettier';
+import nestjsTyped from '@darraghor/eslint-plugin-nestjs-typed';
 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
+  // ...tseslint.configs.strictTypeChecked,
+  // ...tseslint.configs.stylisticTypeChecked,
+  nestjsTyped.configs.flatRecommended,
+  prettierConfig,
+  {
+    rules: {
+      '@darraghor/nestjs-typed/api-method-should-specify-api-response': 'off',
+      '@darraghor/nestjs-typed/controllers-should-supply-api-tags': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
   },
 );
